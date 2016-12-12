e56c1008b6bdae22c7123a9d9c77a0df1073f71a
rakuishi <rakuishi@gmail.com>
2016-12-12 14:37:03 +0900

    Add man.py

---
+++ ch01/man.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+class Man:
+  def __init__(self, name):
+    self.name = name
+    print("Initialized!")
+
+  def hello(self):
+    print("Hello " + self.name + "!")
+
+  def goodbye(self):
+    print("Good-bye " + self.name + "!")
+
+m = Man("David")
+m.hello()
+m.goodbye()
