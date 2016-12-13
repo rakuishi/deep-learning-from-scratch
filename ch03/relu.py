4fe533295efe1a9964e72873e7a04d2c195122c4
rakuishi <rakuishi@gmail.com>
2016-12-13 21:37:27 +0900

    Add relu.py

---
+++ ch03/relu.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def relu(x):
+  return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+
+plt.plot(x, y)
+plt.ylim(-1, 5.5)
+plt.show()
